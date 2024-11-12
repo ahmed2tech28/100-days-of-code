52aa8564fcba53b97e0b1b55a7c38da4d153f707
ahmed2tech28 <120560296+ahmed2tech28@users.noreply.github.com>
2024-11-12 21:57:24 +0500

    day34

---
+++ code/day34/index.js
@@ -0,0 +1,20 @@
+function maxProfit(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
+
+// Test cases
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5
+console.log(maxProfit([7, 6, 4, 3, 1])); // Output: 0
+console.log(maxProfit([1, 2, 3, 4, 5])); // Output: 4
+console.log(maxProfit([2, 4, 1])); // Output: 2
